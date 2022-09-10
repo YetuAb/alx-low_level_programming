782ce4360a8f0ca141f0f113b07c9180251f2044
Yetnayet Abebe <yetnayet.abebe3@gmail.com>
2022-09-09 19:49:37 -0700

    1-5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ * Return: Always (Success)
+ */
+int main(viod)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - reversed alphabet
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char x;
+	
+	for (x = 'z'; x >= 'a'; c--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
