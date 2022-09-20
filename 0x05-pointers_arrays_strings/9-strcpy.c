eb5feaf7b9ab8d263e132d005414908a861c11b8
Yetnayet Abebe <yetnayet.abebe3@gmail.com>
2022-09-20 14:38:25 -0700

    task

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,12 +12,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
 
-	while (*(src + i) != '\n')
+	while (*(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
 		i++;
 	}
-	*(dest + i) = '\n';
+	*(dest + i) = '\0';
 
 	return (dest);
 }
