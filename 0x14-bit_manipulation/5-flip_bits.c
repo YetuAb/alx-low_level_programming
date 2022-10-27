a11a82fa75bb89ab223bf9f9c53f31677964bcf7
Yetnayet Abebe <yetnayet.abebe3@gmail.com>
2022-10-27 14:57:40 -0700

    bit manupulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits 
+ * flip_bits - returns the number of bits
  * needed to flip to get from one number to another
  * @n: number one
  * @m: number two
